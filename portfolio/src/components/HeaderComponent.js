939d643830d192d869de71950476192cac6b8da7
Claire Lu <appsfunlu@gmail.com>
2021-10-21 23:21:22 -0700

    added header component

--- portfolio/src/components/HeaderComponent.js
+++ portfolio/src/components/HeaderComponent.js
@@ -1,13 +1,60 @@
 import React, { Component } from "react";
 
-class Header extends Component {
+export default class Header extends Component {
   render() {
     return (
-      <div>
-        <p>Header</p>
-      </div>
+      <React.Fragment>
+        <header
+          className="sticky-top"
+          style={{
+            backgroundColor: "#000000",
+            textColor: "#ffffff",
+            fontSize: 20,
+            padding:10,
+          }}
+        >
+          <nav id="nav-wrap sticky-top ">
+            <ul id="nav" className="nav">
+              <li className="current">
+                <a
+                  className="m-4"
+                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
+                  href="#home"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  className="mr-4"
+                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
+                  href="#About"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  className="mr-4"
+                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
+                  href="#revies"
+                >
+                  Reviews
+                </a>
+              </li>
+              <li>
+                <a
+
+                  style={{ backgroundColor: "#000000", color: "#ffffff" }}
+                  href="#contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </React.Fragment>
     );
   }
 }
-
-export default Header;
